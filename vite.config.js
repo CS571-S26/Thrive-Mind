ccf3211592896daa4fb5ef3348381bcaaab7da76
Charith Reddy Pareddy <charithpareddy@gmail.com>
2026-08-12 22:21:03 -0400

    Keep /p77/ base path temporarily

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/thrive-mind/',
+  base: '/p77/',
   plugins: [react()],
   build:{
     outDir: "docs"
